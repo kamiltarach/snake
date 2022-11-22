1e8ee6eee84ebb815a38d4586c9e584b2547009c
Kamil Tarach <kamiltarach.kt@gmail.com>
2022-11-22 11:58:47 +0100

    login changes

--- login.php
+++ login.php
@@ -18,22 +18,25 @@
 </head>
 <body>
 <div class="container">
-        <div class="card">
+        <div class="card"><form method="post" action="zaloguj.php">
             <h3>Sing Up</h3>
-            <form method="post" action="zaloguj.php">
                 <div class="inputBox">
-                    <input type="text" required="required">
-                    <span>Username</span>
+                    <input type="text" name="login" required="required">
+                    <span>Nickname</span>
                 </div>
                 <div class="inputBox">
-                    <input type="password" required="required">
-                    <span>Password</span>
+                    <input type="password" name="haslo" required="required">
+                    <span>Haslo</span>
                 </div>
+                <center style="font-size:80%;">
+                    <?php
+                        if(isset($_SESSION['blad']))echo $_SESSION['blad'];
+                    ?>
+                </center>
                 <button>Enter</button>
-            </form>
+            </form></div>
         </div>
-    </div>
-<a href="index.php" style="text-decoration:none;text-align:center;color:blue;font-size:200%;">
+    <a href="index.php" style="text-decoration:none;text-align:center;color:blue;font-size:200%;">
 <h3>Strona główna</h3></a>
 </body>
 </html>
