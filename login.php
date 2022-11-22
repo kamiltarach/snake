f614b5b7f002e414809fb1d2d267f74762e88b14
Kamil Tarach <kamiltarach.kt@gmail.com>
2022-11-22 11:38:57 +0100

    login changes

--- login.php
+++ login.php
@@ -17,22 +17,26 @@
     <title>Snake - zaloguj się</title>
 </head>
 <body>
-    <div id="header">
-        <h1>Zaloguj się do gry Snake</h1><br>
+    <div class="container">
+        <form method="post" action="zaloguj.php">
+            <div class="card">
+                <div class="inputBox">
+                    <input type="text" name="login"><span>Nickname</span><br>
+                </div>
+                <div class="inputBox">
+                    <input type="password" name="haslo"><span>Hasło</span><br>
+                </div>
+                <center style="font-size:80%;">
+                    <?php
+                        if(isset($_SESSION['blad']))echo $_SESSION['blad'];
+                    ?>
+                </center>
+                
+                <br><input type="submit" value="Zaloguj się">
+            </div>
+        </form>
+        <a href="index.php" style="text-decoration:none;text-align:center;color:blue;font-size:200%;">
+        <h3>Strona główna</h3></a>
     </div>
-    <!--<h1 style="text-align:center;border:1px solid black;width:40%;margin:auto;">Strona w trakcie tworzenia</h1>-->
-    <br><br>
-    <form method="post" action="zaloguj.php" id="inputBox">
-            Nickname:<br><input type="text" name="login"><br><br>
-            Hasło:<br><input type="password" name="haslo"><br><br>
-            <center style="font-size:80%;">
-                <?php
-                    if(isset($_SESSION['blad']))echo $_SESSION['blad'];
-                ?>
-            </center>
-            <br><input type="submit" value="Zaloguj się">
-    </form>
-    <a href="index.php" style="text-decoration:none;text-align:center;color:blue;font-size:200%;">
-    <h3>Strona główna</h3></a>
 </body>
 </html>
