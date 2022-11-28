9382a45a45d9f5f3db29fb4cd9ee3df2fc9611d5
Kamil Tarach <kamiltarach.kt@gmail.com>
2022-11-28 12:36:18 +0100

    index and login changes

--- index.php
+++ index.php
@@ -11,13 +11,12 @@
 <body>
     <div id="header">
         <h2>Witaj, chcesz zagrać w Snake'a?</h2>
-    </div>    
-    <div id="login">
-        <ul>
-            <li><a href="login.php"><h1 class="link">Zaloguj się</h1></a></li>
-            <li><a href="register.php"><h1 class="link">Zarejestruj<br>się</h1></a></li>
-        </ul>
     </div>
+        <ul id="login">
+            <li class="link"><a href="login.php"><h1>Zaloguj się</h1></a></li>
+            <li class="link"><a href="register.php"><h1>Zarejestruj się</h1></a></li>
+            <li class="link"><a href="snake-info.php"><h1>info</h1></a></li>
+        </ul>
     <img src="snake1.png">
 </body>
 </html>
